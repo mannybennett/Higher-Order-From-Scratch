950bd552006c2acd82bab05578228e86bd663826
mannybennett <mannybennett99@gmail.com>
2023-02-08 18:36:55 -0500

    corrected

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-Array.prototype.mapFromScratch =
-  function(callBack) {
-    const newArr = []
-    for (let i=0; i < this.length; i++) {
-      newArr.push(callBack(this[i]))
-    }
-    return newArr
+function mapFromScratch(array, callBack) {
+  const newArr = []
+  for (let i=0; i < array.length; i++) {
+    newArr.push(callBack(array[i], i, array))
   }
+  return newArr
+}
 
 const numbers = [1, 2, 3]
 const myFunc = x => x + 1
 
-console.log(numbers.mapFromScratch(myFunc))
+console.log(mapFromScratch(numbers, myFunc))
