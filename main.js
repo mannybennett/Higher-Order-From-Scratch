da10eaf77be810e5d586e0578ca2a708a9cf1636
mannybennett <mannybennett99@gmail.com>
2023-02-08 15:39:35 -0500

    progress

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-class functionsFromScratch {
-    mapFromScratch(callBack) {
-        for (let i=0; i < this.length; i++) {
-            
-        }
+Array.prototype.mapFromScratch =
+  function(callBack) {
+    const newArr = []
+    for (let i=0; i < this.length; i++) {
+      newArr.push(callBack(this[i]))
     }
-}
+    return newArr
+  }
+
+const numbers = [1, 2, 3]
+const myFunc = x => x + 1
+
+console.log(numbers.mapFromScratch(myFunc))
