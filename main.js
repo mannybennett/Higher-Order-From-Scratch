23df8cd569c8ae205f316ac9f94128706ef243c1
mannybennett <mannybennett99@gmail.com>
2023-02-08 22:01:34 -0500

    WIP

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function mapFromScratch(array, callBack) {
   const newArr = []
   for (let i=0; i < array.length; i++) {
-    newArr.push(callBack(array[i], i, array))
+    newArr.push(callBack(array[i]))
   }
   return newArr
 }
@@ -9,4 +9,33 @@ function mapFromScratch(array, callBack) {
 const numbers = [1, 2, 3]
 const myFunc = x => x + 1
 
-console.log(mapFromScratch(numbers, myFunc))
+// console.log(mapFromScratch(numbers, myFunc))
+
+function reduceFromScratch(array, callback, initialValue) {
+  if (!initialValue) {
+    let acc = array[0]
+    for (let i=1; i < array.length; i++) {
+      acc = callback(acc, array[i])
+    }
+    return acc
+  } else {
+    acc = initialValue
+    for (let i=0; i < array.length; i++) {
+      acc = callback(acc, array[i])
+    }
+    return acc
+  }
+}
+
+const myFunc2 = (acc, curr) => acc + curr
+// console.log(reduceFromScratch(numbers, myFunc2, 5))
+
+function filterFromScratch(array, callback) {
+  const newArr = []
+  for (let i=0; i < array.length; i++) {
+    if ('something') {
+      newArr.push(callBack(array[i]))
+    }
+  }
+  return newArr
+}
